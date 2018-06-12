9b822eb2af6b9c0d8bb957cfcab0b1485f7e90df
magmi362 <magnus.mickelsson@ucr.uu.se>
2018-06-12 12:30:02 +0200

    LocalServerPort annotation instead of Value

--- src/test/kotlin/rest/RestApplicationTest.kt
+++ src/test/kotlin/rest/RestApplicationTest.kt
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
@@ -22,7 +23,7 @@ import org.springframework.test.context.junit4.SpringRunner
 class RestApplicationTest {
     val logger = LoggerFactory.getLogger(javaClass)
 
-    @Value("\${local.server.port}")
+    @LocalServerPort
     var port: Int = 0
 
     @Autowired
